dac66abfece5e8ce7b8728757c37b537043b8889
ModsXx <118561210+ModsXx@users.noreply.github.com>
2022-11-24 02:54:54 -0500

    Add files via upload

---
+++ assets/preload/stages/Sexoo.lua
@@ -0,0 +1,11 @@
+function onCreate()
+	
+      makeLuaSprite('Sexoo', 'Sexoo', -800, -300);
+	addLuaSprite('Sexoo', false)
+	setScrollFactor('Sexoo', 1.0, 1.0);
+	
+-- Your Custom bg sprites or something like this
+	
+	 close(true);
+
+end
